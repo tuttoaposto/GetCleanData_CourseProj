0bbd873c8fb30f08c14f1aaec79dc165a351ccb0
tuttoaposto <eunisngor@gmail.com>
2015-05-21 21:57:37 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+#[README for run_analysis.R]
+
+This program summarizes human activity measurements collected by Samsung Galaxy S smartphone	
+obtained here: (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).	
+
+The following steps describe what this script does:	
+
+1. Read in training and test data txt files into R
+
+2. Read in activity labels and measurement variable names into R
+
+3. Attach subject ID to training and test datasets
+
+4. Label measurement variables (561 in total) per each dataset
+
+5. Label each activity:
+    1. WALKING, 
+2. WALKING_UPSTAIRS, 3. WALKING_DOWNSTAIRS, 
+4. SITTING, 
+5. STANDING, 
+6. LAYING	
+
+	
+6. Combine training and test datasets into one (30 subjects in total)
+
+7. Extract just the measurement variables that are the mean and standard deviation of the measurements
+
+8. Summarize the extracted dataset in step 6 with mean by subject and activity 
+   (180 rows and 69 columns in total including activity labels)
+
+9. Output the summarized dataset in step 7 to Avg.txt 
+	
+## For description of each variable in Avg.txt, refer to CodeBook.md
---
+++ run_analysis.R
@@ -0,0 +1,106 @@
+library(data.table)
+library(dplyr)
+
+##Read in activity labels
+Act <- read.table('activity_labels.txt', col.names=c('V1', 'activity'))
+ 
+##Read in feature names
+feat <- fread('features.txt')
+
+#-------------------------------------------------------------------------
+##Read in training dataset
+system.time(tr <- read.table('./X_train.txt', col.names=feat$V2))
+
+##Read in training labels
+system.time(tr_lbl <- fread('./y_train.txt'))
+
+## Read in training dataset subject ID
+system.time(tr_subj <- read.table('./subject_train.txt', col.names='ID'))
+
+## Combine ID, activities, and training data together
+tr <- cbind(tr_subj, tr_lbl, tr)
+head(tr[1:5], 10)
+
+##Label activities
+library(plyr)
+tr <- join(tr, Act, by='V1', type='left')
+table(tr$V1, tr$activity)
+
+## Check data format
+dim(tr)
+head(tr[1:5], 10)
+head(tr[561:564], 10) 
+
+## Assign col names to activity ID
+##:{ I got warnings with this command bc data.table: names(y)[1:3] <- c('ID', 'activity_ID', 'activity')
+setnames(tr, 2, 'activity_ID')
+names(tr)[c(1:5, 561:564)]
+
+###if data.table use these commands:
+### head(tr[, 1:5, with=FALSE], 10)
+##:{ or this command: head(tr[, c('ID', "activity_ID", "activity", "tBodyAcc.mean...X", "tBodyAcc.mean...Y"), with=FALSE], 10)
+
+#-------------------------------------------------------------------------
+
+##Read in testing dataset
+system.time(ts <- read.table('./X_test.txt', col.names=feat$V2))
+
+##Read in testing labels
+system.time(ts_lbl <- fread('./y_test.txt'))
+
+## Read in testing dataset subject ID
+system.time(ts_subj <- read.table('./subject_test.txt', col.names='ID'))
+
+## Combine ID, activities, testing data together
+ts <- cbind(ts_subj, ts_lbl, ts)
+head(ts[1:5], 10)
+
+##Label activities
+ts <- join(ts, Act, by='V1', type='left')
+table(ts$V1, ts$activity)
+
+## Check data format
+dim(ts)
+head(ts[1:5], 10)
+head(ts[561:564], 10)
+
+## Assign col name to activity ID
+setnames(ts, 2, 'activity_ID')
+names(ts)[c(1:5, 561:564)]
+
+#-------------------------------------------------------------
+
+##Combine training and test sets
+all <- rbind(tr,ts)
+
+## Order dataset by ID and activity
+all <- arrange(all, ID, activity_ID)
+
+##Check data
+dim(all)	#10299 * 564
+head(all[1:5], 100)
+
+## Select vars that say mean() and std()
+### mean() and std() become mean.., std..
+### double backslashes to escape ..
+meanstd <- all[, grepl('(mean|std)\\.{2}|ID|activity_ID|activity', names(all))]  
+
+## Get average of each column by subject and activity
+Avg <- aggregate(meanstd[,3:68], by=list(ID = meanstd$ID, activity_ID = meanstd$activity_ID), 
+                  FUN=mean, na.rm=TRUE)
+
+dim(Avg) #180 * 68
+
+## Tag activity name
+Avg <- merge(Act, Avg, by.x='V1', by.y='activity_ID', all.y=TRUE)
+setnames(Avg, 1,'activity_ID')
+Avg[1:6, 1:6]
+
+## Write dataset to .txt
+write.table(Avg, './Avg.txt', row.name=FALSE)
+
+## Or to write dataset to .csv
+##write.csv(meanstd, file = "./write.csv", quote = FALSE) 
+
+##read data back to R: 
+##x <- read.table('./Avg.txt', header=TRUE)
